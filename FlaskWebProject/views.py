efc0e42edbe8ee6f85a2572d9e69ade9134f015e
norieanne <norie124@yahoo.com>
2025-02-24 12:50:50 +0800

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -66,13 +66,13 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            # log unsuccessful login
-            app.logger.info(f'Unsuccessful login attempt for user: {form.username.data}')
+            # for unsuccessful login
+            app.logger.info(f'sorry unsuccessful login: {form.username.data}')
             flash('Invalid username or password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        # log successful login
-        app.logger.info(f'Successful login for user: {form.username.data}')
+        # for successful login
+        app.logger.info(f'Successful login: {form.username.data}')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
